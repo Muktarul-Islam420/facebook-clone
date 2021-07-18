3d0c7ad01a26b7fce06913498cedb46a08e4f691
khan4200000 <www.muktarul420@gmail.com>
2021-07-18 09:50:26 +0600

    facebook app virson one done

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -5,6 +5,9 @@ import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import { useStateValue } from './StateProvider';
+import db from './firebase';
+import firebase from 'firebase';
+
 function MessageSender() {
     const [{user}, dispatch] = useStateValue();
     const [input, setInput] = useState("");
@@ -14,6 +17,14 @@ function MessageSender() {
         e.preventDefault();
 
         // some clever db stuff to
+        db.collection("posts").add({
+            message:input,
+            timestamp:firebase.firestore.FieldValue.
+            serverTimestamp(),
+            profilePic:user.photoURL,
+            username:user.displayName,
+            image:imageUrl,
+        })
 
         setInput("");
         setImageUrl("");
